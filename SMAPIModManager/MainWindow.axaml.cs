3fc2c3d1061b3e03a36f2d230b1e5a737743b909
jack-bkr <jackbarker04@icloud.com>
2025-03-07 13:08:14 +0000

    button

--- SMAPIModManager/MainWindow.axaml.cs
+++ SMAPIModManager/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
+using System;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace SMAPIModManager;
 
@@ -7,5 +9,10 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    public void onButtonPress(object sender, RoutedEventArgs e)
+    {
+        Console.WriteLine("Button pressed!"); 
     }
 }
